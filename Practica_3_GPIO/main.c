f6ddc1a35d06b717d33f40d4289ec8ce00d3969f
S-Bou <bouyo30@gmail.com>
2020-02-22 10:20:15 +0100

    2202201020

--- Practica_3_GPIO/main.c
+++ Practica_3_GPIO/main.c
@@ -1,144 +1,95 @@
-/**
-   @file main.c
+#include <stdint.h>
+#include <stm32f4xx.h> 
+#include <stm32f4xx_gpio.h> 
+#include <stm32f4xx_rcc.h> 
 
-   @brief Plantilla Keil 5 para la placa STM32F$Discovery
-   Más detalles en el archivo leeme.txt
-   
-   @author Equipo ARM Power http://armpower.blogs.upv.es/
-   @date 2017/03/06
-*/
+uint8_t cara = 1;            //    ccc-cccc 
+uint8_t figuras[6] = {
+											0x08,  //1 - 000-   1000  
+											0x11,  //2 - 001-   0001  
+											0x19,  //3 - 001-   1001
+											0x55,  //4 - 101-   0101
+											0x5D,  //5 - 101-   1101
+											0x77,  //6 - 111-   0111 
+											}; 
 
-#include <stdio.h>
-#include <stm32f4xx.h>
-/**
-  ******************************************************************************
-  * @file    RCC/main.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    19-September-2011
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************  
-  */ 
-
-/* Includes ------------------------------------------------------------------*/
-#include "stm32f4_discovery.h"
-
-/** @addtogroup STM32F4_Discovery_Peripheral_Examples
-  * @{
-  */
-
-/** @addtogroup RCC_Example
-  * @{
-  */ 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/ 
-/* Private function prototypes -----------------------------------------------*/
-void Delay (uint32_t nCount);
-
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief   Main program
-  * @param  None
-  * @retval None
-  */
-int main(void)
+void iniciarPulsador(void) 
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  NVIC_InitTypeDef NVIC_InitStructure;
-  RCC_ClocksTypeDef RCC_ClockFreq;
-  
-  /*!< At this stage the microcontroller clock setting is already configured, 
-  this is done through SystemInit() function which is called from startup
-  file (startup_stm32f4xx.s) before to branch to application main.
-  To reconfigure the default setting of SystemInit() function, refer to
-  system_stm32f4xx.c file
-  */     
-
-  /* Initialize LEDs mounted on STM32F4-Discovery board ***************************/
-  STM_EVAL_LEDInit(LED4);
-  STM_EVAL_LEDInit(LED3);
-  STM_EVAL_LEDInit(LED5);
-  STM_EVAL_LEDInit(LED6);
-  
-  /* Turn on LED4 and LED5 */
-  STM_EVAL_LEDOn(LED4);
-  STM_EVAL_LEDOn(LED5);
-  
-  /* This function fills the RCC_ClockFreq structure with the current
-  frequencies of different on chip clocks (for debug purpose) **************/
-  RCC_GetClocksFreq(&RCC_ClockFreq);
-  
-  /* Enable Clock Security System(CSS): this will generate an NMI exception
-  when HSE clock fails *****************************************************/
-  RCC_ClockSecuritySystemCmd(ENABLE);
-  
-  /* Enable and configure RCC global IRQ channel, will be used to manage HSE ready 
-     and PLL ready interrupts. 
-     These interrupts are enabled in stm32f4xx_it.c file **********************/
-  NVIC_InitStructure.NVIC_IRQChannel = RCC_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;  
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-
-  /* Output clock on MCO2 pin(PC9) ****************************************/ 
-  /* Enable the GPIOC peripheral */ 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-  
-  /* Configure MCO2 pin(PC9) in alternate function */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-    
-  /* System clock selected to output on MCO2 pin(PC9)*/
-  RCC_MCO2Config(RCC_MCO2Source_SYSCLK, RCC_MCO2Div_2);
-
-  while (1)
-  {
-    /* Toggle LED3 and LED6 */
-    STM_EVAL_LEDToggle(LED3);
-    STM_EVAL_LEDToggle(LED6);
-
-    /* Insert a delay */
-    Delay(0xFFFFFF);
-
-    /* Toggle LED4 and LED5 */
-    STM_EVAL_LEDToggle(LED4);
-    STM_EVAL_LEDToggle(LED5);
-
-    /* Insert a delay */
-    Delay(0xFFFFFF);    
-  }
+	GPIO_InitTypeDef puerto;   
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);  
+	puerto.GPIO_Pin  = GPIO_Pin_0; 
+	puerto.GPIO_Mode = GPIO_Mode_IN; 
+	puerto.GPIO_PuPd = GPIO_PuPd_NOPULL;   
+	GPIO_Init(GPIOA, &puerto); 
+} 
+	
+void led_conf(void) //configura apropiadamente la GPIO o
+{
+	GPIO_InitTypeDef puerto;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);  
+	puerto.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	puerto.GPIO_Mode  = GPIO_Mode_OUT;
+	puerto.GPIO_OType = GPIO_OType_PP; 
+	puerto.GPIO_Speed = GPIO_Speed_100MHz;  
+	puerto.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &puerto);	
 }
 
-/**
-  * @brief  Inserts a delay time.
-  * @param  nCount: specifies the delay time length.
-  * @retval None
-  */
-void Delay(__IO uint32_t nCount)
+void led_on(void)  //enciende el led naranja o
 {
-  /* Decrement nCount value */
-  while (nCount != 0)
-  {
-    nCount--;
-  }
+ GPIO_SetBits(GPIOD, GPIO_Pin_12);
 }
+void led_off(void) //apaga el led naranja 
+{
+ GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+}
+
+uint8_t estadoPulsador(void) {   
+	
+	return GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0); 
+} 
+
+void visualizarDado(uint8_t c) 
+{
+   GPIO_Write(GPIOD, figuras[c - 1]); 
+} 
+
+
+int main(void) {
+		
+		int r;
+	int j;
+	led_conf();
+  iniciarPulsador(); 
+	r=0;
+	
+	while(1){
+	
+		if (estadoPulsador()) {
+											       r=(r+1)%5;
+														 for (j=0;j<2000000;j++) {};
+														 while(estadoPulsador());
+														 for (j=0;j<2000000;j++) {};
+														  
+		}
+		led_on();
+		for (j=0;j<1000000+2000000*r;j++) {};
+		led_off();
+		for (j=0;j<1000000+2000000*r;j++) {};		
+	}
+		return(0); 
+}
+
+
+
+
+//	int j;
+//	led_conf();
+
+//	while(1)
+//{
+//		led_on();
+//		for (j=0;j<10000000;j++) {};
+//		led_off();
+//		for (j=0;j<10000000;j++) {};
+//}
